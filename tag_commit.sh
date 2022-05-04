61e9348b84a280e98d13f8feb0fc72845f349de7
hgosansn <hugo@agorapulse.com>
2022-05-04 02:40:59 +0200

    Github action
    remove those pesky node modules
    
    
    Github action
    
    Git cleaning

--- tag_commit.sh
+++
@@ -1,7 +0,0 @@
-
-
-git rm -r --cached .
-git add .
-
-git commit -m "${1}"
-git push -u origin main --tags
--- webpack.prod.js
+++
@@ -1,8 +0,0 @@
-const merge = require('webpack-merge') 
-const common = require('./webpack.config.js');
-
-prod = true;
-
-module.exports = merge(common, {
-  mode: 'production',
-});
